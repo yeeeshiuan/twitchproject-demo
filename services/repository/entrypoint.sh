4e5d6b03383e9349f483975e9393ea8090c2de10
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-02-16 21:32:58 +0800

    Update repository(wait for db)

--- services/repository/entrypoint.sh
+++ services/repository/entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+echo "Waiting for postgres..."
+while ! nc -z repository-db 27017; do
+  sleep 0.1
+done
+
+echo "PostgreSQL started"
+
+python manage.py create-user
+python manage.py seed-db
 
 python manage.py run -h 0.0.0.0
